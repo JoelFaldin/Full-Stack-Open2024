14926bffaa57155386b5d2e332a9d0d46e62392d
JoelFaldin <joelfaldin@gmail.com>
2023-11-16 00:06:30 -0300

    Minor fixes, turned state data array into a simple array

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,25 +4,26 @@ import AddPeople from './components/AddPeople'
 import Filter from './components/Filter'
 
 function App() {
-  const [persons, setPersons] = useState([
+  const person = [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  ]
+  
   const [newName, setNewName] = useState('Enter a name...')
   const [newNumber, setNewNumber] = useState('Enter a number...')
   const [show, setShow] = useState('')
-  const [personsToShow, setPersonsToShow] = useState([...persons])
+  const [personsToShow, setPersonsToShow] = useState([...person])
 
   const submitPerson = (event) => {
     event.preventDefault()
-    const index = persons.findIndex(person => person.name === newName)
+    const index = person.findIndex(person => person.name === newName)
     if (index === -1) {
       const newPerson = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1
+        id: person.length + 1
       }
       setPersonsToShow(oldPersons => [...oldPersons, newPerson])  
       setNewName('')
@@ -41,12 +42,8 @@ function App() {
     setShow(event.target.value)
   }
 
-  const handleTest = () => {
-    console.log('this is a test')
-  }
-
   useEffect(() => {
-    setPersonsToShow(persons.filter(person => person.name.includes(show)))
+    setPersonsToShow(person.filter(person => person.name.includes(show)))
   }, [show])
 
   return (
