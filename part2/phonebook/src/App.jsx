c8e056c8c550c729cdd1c131df0c58186039b7d9
JoelFaldin <joelfaldin@gmail.com>
2023-11-15 22:57:14 -0300

    search filter implemented (2.9)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-123456'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('Enter a name...')
   const [newNumber, setNewNumber] = useState('Enter a number...')
+  const [show, setShow] = useState('')
 
   const submitPerson = (event) => {
     event.preventDefault()
-    // console.log(event.target.name.value)
     const index = persons.findIndex(person => person.name === newName)
     if (index === -1) {
       const newPerson = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(oldPersons => [...oldPersons, newPerson])  
       setNewName('')
@@ -26,17 +27,29 @@ function App() {
       alert(`${newName} is already added to the phonebook!!!`)
     }
   }
-
   const handleName = (event) => {
     setNewName(event.target.value)
   }
   const handleNumber = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleChange = (event) => {
+    setShow(event.target.value)
+  }
+
+  useEffect(() => {
+    const personsToShow = persons.filter(person => person.name.includes(show))
+    setPersons(personsToShow)
+    // console.log(personsToShow)
+  }, [show])
 
   return (
     <>
       <h1>PhoneBook</h1>
+
+      <input placeholder='Enter a person' onChange={handleChange} />
+
+      <h2>Add a new person:</h2>
       <form onSubmit={submitPerson}>
         <span htmlFor="name">name:</span>
         <input placeholder={newName} onChange={handleName} id="name" />
@@ -47,10 +60,11 @@ function App() {
           <button type='submit'>Add person</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+
+      <h3>Numbers</h3>
       {persons.map(person => {
         return (
-          <p key={person.name}>{person.name} {person.number}</p>
+          <p key={person.id}>{person.name} {person.number}</p>
         )
       })}
     </>
