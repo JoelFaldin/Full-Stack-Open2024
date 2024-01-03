f2afbd5b2218bb28cb0883f60dd5ddf273b4089e
JoelFaldin <joelfaldin@gmail.com>
2024-01-03 19:40:02 -0300

    feat: added Node app, returns a  hardcoded list (3.1)

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello there!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(phonebook)
+})
+
+const port = 3001
+app.listen(port, () => {
+    console.log('Server up! 🪄')
+    console.log('http://localhost:3001')
+    console.log('http://localhost:3001/api/persons')
+})
