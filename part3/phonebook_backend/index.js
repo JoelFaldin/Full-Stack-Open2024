1ef7f158abe52b77a59ba406cc66d31734e6440b
JoelFaldin <joelfaldin@gmail.com>
2024-01-04 00:33:13 -0300

    feat: added delete request (3.4)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -52,7 +52,13 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    phonebook = phonebook.filter(p => p.id !== id)
     
+    res.status(204).end()
 })
 
 const port = 3001
