3da6aaa4b5637a9ef7a48cf2907ab4d98a75f267
JoelFaldin <joelfaldin@gmail.com>
2024-01-04 12:56:45 -0300

    refactor: changed Morgan config to show data sent in POST (3.8)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,7 +3,16 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
+}))
 
 let phonebook = [
     { 
