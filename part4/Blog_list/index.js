06b02205a10a3e7276c26c7135f34169849bc3ce
JoelFaldin <joelfaldin@gmail.com>
2024-01-11 22:24:36 -0300

    refactor: refactoring the project into different modules (4.2)

--- part4/Blog_list/index.js
+++ part4/Blog_list/index.js
@@ -4,6 +4,6 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-    logger.data(`Server running in port ${config.PORT}`)
+    logger.data(`Port used: ${config.PORT}`)
     logger.data(`http://localhost:${config.PORT}/api/blogs`)
 })
