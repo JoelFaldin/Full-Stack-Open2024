a794c20faca00d541f6243722347c4971389b098
JoelFaldin <joelfaldin@gmail.com>
2024-04-22 15:21:28 -0400

    feat: adding a button to show more details of each blog (5.7)

--- part5/bloglist_frontend/src/components/Blog.jsx
+++ part5/bloglist_frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false)
+
+  return !viewDetails ? (
+    <div>
+      {blog.title}
+      <button onClick={() => setViewDetails(true)}>show details</button>
+    </div>
+  ): (
+    <div className="details">
+      <p>{blog.title}</p>
+      <p>{blog.url}</p>
+      <p>
+        {blog.likes} likes
+        <button>like</button>
+      </p>
+      <p>{blog.author}</p>
+      <button onClick={() => setViewDetails(false)}>hide details</button>
+    </div>
+  )
+}
 
 export default Blog
