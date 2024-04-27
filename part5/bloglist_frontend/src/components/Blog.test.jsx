b20fa503cf1c4c5f4dd921fcf4294ccf3be3129f
JoelFaldin <joelfaldin@gmail.com>
2024-04-26 22:58:11 -0400

    feat: testing Blog component when clicking button (5.14)!

--- part5/bloglist_frontend/src/components/Blog.test.jsx
+++ part5/bloglist_frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title and author', async () => {
@@ -29,4 +30,34 @@ test('renders blog title and author', async () => {
     expect(blogAuthor).toBeDefined()
     expect(blogURL).toBeNull()
     expect(blogLikes).toBeNull()
+})
+
+test('renders blog url and number of likes when clicking on "show details"', async () => {
+    const blogData = {
+        author: 'John M.',
+        id: '...',
+        likes: 0,
+        url: 'test.com',
+        user: {
+            username: 'userTest',
+            name: 'superTester',
+            id: '...2'
+        }
+    }
+
+    const mockFunction = () => {
+        return
+    }
+
+    const { container } = render(<Blog blog={blogData} userName='' blogs={[blogData]} setBlogs={mockFunction} handleMessages={mockFunction} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('show details')
+    await user.click(button)
+
+    const blogURL = container.querySelector('.blog-url')
+    const blogLikes = container.querySelector('.blog-likes')
+
+    expect(blogURL).toBeDefined()
+    expect(blogLikes).toBeDefined()
 })
