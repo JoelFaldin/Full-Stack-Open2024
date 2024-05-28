bfbc4259e3f382b9f411f0eddc6263e0ae12c0da
JoelFaldin <joelfaldin@gmail.com>
2024-05-28 14:21:15 -0400

    refactor: migrating total likes test, 4.4

--- part4/Blog_list/tests/totalLikes.test.js
+++ part4/Blog_list/tests/totalLikes.test.js
@@ -1,19 +1,22 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+
 const listhelper = require('../utils/list_helper')
 
-describe('total of likes', () => {
-    const oneBlogList = [
+describe('total likes', () => {
+    const listWithOneBlog = [
         {
           _id: '5a422aa71b54a676234d17f8',
           title: 'Go To Statement Considered Harmful',
           author: 'Edsger W. Dijkstra',
-          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
           likes: 5,
           __v: 0
-        },
-    ]
+        }
+      ]
 
     test('with only one blog, it equals the likes of that', () => {
-        const result = listhelper.totalLikes(oneBlogList)
-        expect(result).toBe(5)
+        const result = listhelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
     })
 })
