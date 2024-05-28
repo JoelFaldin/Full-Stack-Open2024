5f86e380ab194138e01c0c950ceced64db5ee56f
JoelFaldin <joelfaldin@gmail.com>
2024-05-28 14:13:41 -0400

    refactor: migrating dummy test, 4.3

--- part4/Blog_list/tests/dummy.test.js
+++ part4/Blog_list/tests/dummy.test.js
@@ -1,8 +1,10 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
 const listhelper = require('../utils/list_helper')
 
 test('dummy always returns 1', () => {
     const blogs = []
 
     const result = listhelper.dummy(blogs)
-    expect(result).toBe(1)
+    assert.strictEqual(result, 1)
 })
