64a3ddb3a97bae85e216b36eae00ffac2849f754
JoelFaldin <joelfaldin@gmail.com>
2024-06-17 00:34:16 -0400

    feat: adding app functionality, 6.2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -7,23 +7,41 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+const good = () => {
     store.dispatch({
-      type: 'GOOD'
+        type: 'GOOD'
     })
-  }
+    }
 
-  return (
+    const ok = () => {
+    store.dispatch({
+        type: 'OK'
+    })
+    }
+
+    const bad = () => {
+    store.dispatch({
+        type: 'BAD'
+    })
+    }
+
+    const reset = () => {
+    store.dispatch({
+        type: 'ZERO'
+    })
+    }
+
+    return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+        <button onClick={good}>good</button>
+        <button onClick={ok}>ok</button> 
+        <button onClick={bad}>bad</button>
+        <button onClick={reset}>reset stats</button>
+        <div>good {store.getState().good}</div>
+        <div>ok {store.getState().ok}</div>
+        <div>bad {store.getState().bad}</div>
     </div>
-  )
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
