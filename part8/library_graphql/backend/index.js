5128b8edb298ec238146c496fb7ff4255240022c
JoelFaldin <joelfaldin@gmail.com>
2024-07-26 08:53:17 -0400

    refactor: fixing all queries and mutations to work with the database, 8.14

--- part8/library_graphql/backend/index.js
+++ part8/library_graphql/backend/index.js
@@ -11,84 +11,6 @@ mongoose.set('strictQuery', false)
 
 require('dotenv').config()
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
 const MONGO_URI = process.env.MONGO_URI
 console.log('connecting to: ', MONGO_URI)
 
@@ -104,7 +26,7 @@ const typeDefs = `
   type Author {
     name: String!
     born: Int
-    id: ID!
+    _id: ID!
     bookCount: Int
   }
 
@@ -139,22 +61,41 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
+    bookCount: async () => Book.countDocuments(),
+    authorCount: async () => Author.countDocuments(),
     allBooks: async (root, args) => {
-      // let filteredBooks = books
+      try {
+        let filteredBooks = await Book.find({}).populate('author').exec()
+        
+        // if (args.author) {
+        //   filteredBooks = filteredBooks.filter(b => b.author.name === args.author)
+        // }
+        if (args.genre) {
+          filteredBooks = await Book.find({ genres: args.genre }).populate('author').exec()
+        }
 
-      // if (args.author) {
-      //   filteredBooks = filteredBooks.filter(b => b.author === args.author)
-      // }
-      // if (args.genre) {
-      //   filteredBooks = filteredBooks.filter(b => b.genres.includes(args.genre))
-      // }
-
-      const books = await Book.find({}).populate('author').exec()
-      return books
+        return filteredBooks
+      } catch (error) {
+        throw new GraphQLError('Couldnt retrieve books from the database', {
+          code: 'BAD_USER_INPUT',
+          error
+        }) 
+      }
     },
-    allAuthors: () =>  authors.map(author => ({ ...author, bookCount: books.filter(b => b.author === author.name).length }))
+    allAuthors: async () => {
+      try {
+        const authors = await Author.find({})
+        const books = await Book.find({})
+        
+        const finalAuthors = authors.map(author => ({ ...author.toObject(), bookCount: books.filter(b => b.author.toString() === author._id.toString()).length }))
+        return finalAuthors
+      } catch (error) {
+        throw new GraphQLError('Couldnt retrieve authors from the database', {
+          code: 'BAD_USER_INPUT',
+          error
+        })
+      }
+    }
   },
   Mutation: {
     addBook: async (root, args) => {
@@ -183,19 +124,21 @@ const resolvers = {
         })
       }
     },
-    editAuthor: (root, args) => {
-      const searchAuthor = authors.find(a => a.name === args.name)
-      if (!searchAuthor) {
-        return null
-      }
+    editAuthor: async (root, args) => {
+      try {
+        const user = await Author.findOne({ name: args.name })
+        if (!user) {
+          return null
+        }
 
-      const newAuthor = {
-        ...searchAuthor,
-        born: args.setBornTo
+        const newAuthor = await Author.findOneAndUpdate({ _id: user._id }, { born: args.setBornTo }, { new: true })
+        return newAuthor
+      } catch (error) {
+        throw new GraphQLError('Couldnt update author birthday', {
+          code: 'BAD_USER_INPUT',
+          error
+        })
       }
-
-      authors = authors.map(a => a.name === args.name ? newAuthor : a)
-      return newAuthor
     }
   }
 }
